8cc6d0749edadd37cd8d2301458e78f2aa8c8a62
Caste <www.jrctabora@gmail.com>
2022-09-27 20:54:58 -0600

    📅 27-09-22 Herencia, Extendes, Interfaces

--- oop/Demo/Vista.php
+++ oop/Demo/Vista.php
@@ -5,12 +5,21 @@ class Vista {
     public $strFechaNacimiento;
     public $intNumero;
 
+    public function __construct($strNombre="",$strPais="",$intNumero="",$strFechaNacimiento=""){
+        $this->strNombre = $strNombre;
+        $this->strPais = $strPais;
+        $this->intNumero = $intNumero;
+        $this->strFechaNacimiento = $strFechaNacimiento;
+    }
+
     public function printDatos(){
         echo $this->strNombre ." "
             .$this->strPais   ." "
             .$this->intNumero ." "
             .$this->strFechaNacimiento ." ";
     }
+
+    
 }
 
 ?>
--- oop/index.php
+++ oop/index.php
@@ -1,13 +1,15 @@
 <?php 
 require_once "Demo/Vista.php";
+require_once "Demo/Rectangle.php";
+require_once "Demo/Square.php";
 
-$fulanito = new Vista();
+$fulanito = new Vista("Fulanito de Tal C Desde El Constructor", "Honduras","10","20201209");
 $menganito = new Vista();
 
-$fulanito->strNombre = "Fulanito de tal";
+/* $fulanito->strNombre = "Fulanito de tal";
 $fulanito->intNumero = 1;
 $fulanito->strPais = "Honduras";
-$fulanito->strFechaNacimiento = "2002-02-19";
+$fulanito->strFechaNacimiento = "2002-02-19"; */
 
 $menganito->strNombre = "Menganito de tal";
 $menganito->intNumero = 2;
@@ -17,5 +19,18 @@ $menganito->strFechaNacimiento = "2000-12-24";
 echo $fulanito->printDatos();
 echo "</br>";
 echo $menganito->printDatos();
+echo "<hr>";
+$rect = new Rectangle(5,10);
+$square = new Square(10);
 
+$arrForms = array($rect, $square);
+
+foreach($arrForms as $form){
+    echo $form->getArea();
+    echo "</br>";
+    echo $form->getPerimetro();
+    echo "<hr>";
+}
+//TODO: Tarea Clase del circulo area(pi*r^2) y perimetro(2*pi*r)
 ?>
+
